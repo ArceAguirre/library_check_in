8e06ef83769da1432d8f7ab57ac0b8d927a85972
Arce <arce.aguirre.trevino@gmail.com>
2018-02-02 16:45:23 -0600

    Nuevo proyecto

---
+++ library_check_in/library_check_in/frm_login.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace library_check_in
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_login));
+            this.img_logoFCFM = new System.Windows.Forms.Panel();
+            this.img_logoUANL = new System.Windows.Forms.Panel();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_password = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_login = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // img_logoFCFM
+            // 
+            this.img_logoFCFM.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("img_logoFCFM.BackgroundImage")));
+            this.img_logoFCFM.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.img_logoFCFM.Location = new System.Drawing.Point(206, 12);
+            this.img_logoFCFM.Name = "img_logoFCFM";
+            this.img_logoFCFM.Size = new System.Drawing.Size(80, 80);
+            this.img_logoFCFM.TabIndex = 25;
+            // 
+            // img_logoUANL
+            // 
+            this.img_logoUANL.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("img_logoUANL.BackgroundImage")));
+            this.img_logoUANL.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.img_logoUANL.Location = new System.Drawing.Point(12, 12);
+            this.img_logoUANL.Name = "img_logoUANL";
+            this.img_logoUANL.Size = new System.Drawing.Size(80, 80);
+            this.img_logoUANL.TabIndex = 24;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(71, 126);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(44, 13);
+            this.lbl_name.TabIndex = 27;
+            this.lbl_name.Text = "Nombre";
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Location = new System.Drawing.Point(54, 152);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(61, 13);
+            this.lbl_password.TabIndex = 28;
+            this.lbl_password.Text = "Contraseña";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(121, 123);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 29;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(121, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 30;
+            // 
+            // btn_login
+            // 
+            this.btn_login.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btn_login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_login.Location = new System.Drawing.Point(121, 175);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(100, 23);
+            this.btn_login.TabIndex = 31;
+            this.btn_login.Text = "Ingresar";
+            this.btn_login.UseVisualStyleBackColor = false;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // frm_login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(298, 236);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_password);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.img_logoFCFM);
+            this.Controls.Add(this.img_logoUANL);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_login";
+            this.Text = "Entrada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel img_logoFCFM;
+        private System.Windows.Forms.Panel img_logoUANL;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_password;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_login;
+    }
+}
+
---
+++ library_check_in/library_check_in/frm_login.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+/****************************************************/
+/*  Author      |   Arcelia Aguirre                 */
+/*  Date        |   02-02-2018                      */
+/*  Description |   Pantalla de login               */
+/****************************************************/
+namespace library_check_in
+{
+    public partial class frm_login : Form{
+        public frm_login(){
+            InitializeComponent();
+        }
+
+        private void btn_login_Click(object sender, EventArgs e){
+
+        }
+    }
+}
