7ca2a556386c9fb5dcaedebb476f7820ec40c3b6
ArceAguirre <arce.aguirre.trevino@gmail.com>
2018-04-16 16:17:37 -0500

    Validación en linea 48

--- library_check_in/library_check_in/frm_login.cs
+++ library_check_in/library_check_in/frm_login.cs
@@ -8,124 +8,126 @@ using System.Text;
 using System.Windows.Forms;
 using System.Data.SqlClient;
 using library_check_in.User_CICE;
-
-/****************************************************/
-/*  Author      |   Arcelia Aguirre                 */
-/*  Date        |   02-02-2018                      */
-/*  Description |   Pantalla de login               */
-/****************************************************/
+/**
+ *  Author      | Arcelia Aguirre
+ *  Date        | 02-02-2018
+ *  Description | Pantalla de login
+ */
 namespace library_check_in{
     public partial class frm_login : Form
     {
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   09-03-2018                      */
-        /*  Description |   Constructor del form            */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre 
+         *  Date        | 09-03-2018
+         *  Description | Constructor del form
+         */
         public frm_login()
         {
             InitializeComponent();
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   09-03-2018                      */
-        /*  Description |   Funcion al iniciar sesion       */
-        /*  Parameteres |   object sender, EventArgs e      */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 09-03-2018
+         *  Description | Funcion al iniciar sesion
+         *  Parameteres | object sender, EventArgs e
+         */
         private void btn_login_Click(object sender, EventArgs e)
         {
-            login();
+            Login();
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   09-03-2018                      */
-        /*  Description |   Funcion al iniciar sesion       */
-        /****************************************************/
-        private void login()
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 09-03-2018
+         *  Description | Funcion al iniciar sesion
+         */
+        private void Login()
         {
             DataTable dt = new DataTable();
             UserCICE userCICE = new UserCICE();
 
-            dt =  userCICE.login(txt_name.Text, txt_password.Text);
-            if (dt.Rows.Count == 0)
+            /*Validación*/
+            if (txt_name.Text == "")
             {
-                MessageBox.Show("Usuario o Password Incorrectos");
+                MessageBox.Show("El nombre no puede estar vacío");
+                return;
             }
-            else
-            {
-                frm_signIn frm_checkIn = new frm_signIn();
-                frm_checkIn.Show();
-            }
-        }
 
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   28-02-2018                      */
-        /*  Description |   Funcion al inicar la pantalla   */
-        /*  Parameteres |   object sender, EventArgs e      */
-        /****************************************************/
+            dt =  userCICE.login(txt_name.Text, txt_password.Text);
+            if (dt != null)
+            {
+                if (dt.Rows.Count == 0)
+                    MessageBox.Show("Usuario o Password Incorrectos");
+                else
+                {
+                    frm_signIn frm_checkIn = new frm_signIn();
+                    frm_checkIn.Show();
+                }
+            }
+            /*Quitar*/
+            frm_signIn frm_checkIn2 = new frm_signIn();
+            frm_checkIn2.Show();
+        }
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 28-02-2018
+         *  Description | Funcion al inicar la pantalla
+         *  Parameteres | object sender, EventArgs e
+         */
         private void frm_login_Load(object sender, EventArgs e)
         {
             this.txt_name.Text = "Usuario";
             this.txt_password.Text = "password";
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   28-02-2018                      */
-        /*  Description |   Placeholder                     */
-        /*  Parameteres |   object sender,                  */
-        /*                  KeyPressEventArgs e             */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 28-02-2018
+         *  Description | Placeholder
+         *  Parameteres | object sender, KeyPressEventArgs e             
+         */
         private void txt_name_KeyPress(object sender, KeyPressEventArgs e)
         {
             if(this.txt_name.Text == "Usuario")
                 this.txt_name.Text = "";
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   28-02-2018                      */
-        /*  Description |   Placeholder                     */
-        /*  Parameteres |   object sender, KeyEventArgs e   */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 28-02-2018
+         *  Description | Placeholder
+         *  Parameteres | object sender, KeyEventArgs e   
+         */
         private void txt_name_KeyUp(object sender, KeyEventArgs e)
         {
             if (this.txt_name.Text == "")
                 this.txt_name.Text = "Usuario";
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   28-02-2018                      */
-        /*  Description |   Placeholder                     */
-        /*  Parameteres |   object sender, KeyEventArgs e   */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 28-02-2018
+         *  Description | Placeholder
+         *  Parameteres | object sender, KeyEventArgs e
+         */
         private void txt_password_KeyPress(object sender, KeyPressEventArgs e)
         {
             //if (this.txt_password.Text == "password")
             //    this.txt_password.Text = "";
         }
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   28-02-2018                      */
-        /*  Description |   Placeholder                     */
-        /*  Parameteres |   object sender, KeyEventArgs e   */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 28-02-2018
+         *  Description | Placeholder
+         *  Parameteres | object sender, KeyEventArgs e
+         */
         private void txt_password_KeyUp(object sender, KeyEventArgs e)
         {
             //if (this.txt_password.Text == "")
             //    this.txt_password.Text = "password";
         }
-
-        /****************************************************/
-        /*  Author      |   Arcelia Aguirre                 */
-        /*  Date        |   09-03-2018                      */
-        /*  Description |   Función para cerrar el form     */
-        /*  Parameteres |   object sender, EventArgs e      */
-        /****************************************************/
+        /**
+         *  Author      | Arcelia Aguirre
+         *  Date        | 09-03-2018
+         *  Description | Función para cerrar el form
+         *  Parameteres | object sender, EventArgs e
+         */
         private void cbtn_closeLogin_Click(object sender, EventArgs e)
         {
             Close();
